75ac36742d08884c343c1f966a3f24a0a825a6c5
Abasibiangake <48968451+Abasibiangake@users.noreply.github.com>
2022-06-22 10:59:03 +0100

    refactored code
    made updates to the controller:
    *update processAddPage to include create new car.
    * fixed and completed logic for the edit button

--- controllers/car.js
+++ controllers/car.js
@@ -69,21 +69,76 @@ module.exports.processAddPage = (req, res, next) => {
         color: req.body.color,
         price: req.body.price  
 
-    })
+    });
+
+    CarModel.create(newCar, (err, car) =>{
+        if(err)
+        {
+            console.log(err);
+            res.end(err);
+        }
+        else
+        {
+            // refresh the book list
+            console.log(car);
+            res.redirect('/cars/list');
+        }
+    });
 
 }
 
 // Gets a car by id and renders the Edit form using the add_edit.ejs template
 module.exports.displayEditPage = (req, res, next) => {
     // ADD YOUR CODE HERE
+    let id = req.params.id;
 
+    CarModel.findById(id, (err, carToEdit) => {
+        if (err){
+            console.log(err);
+            res.end(err);
+        }
+        else
+        {
+            //show the edit view
+            res.render('cars/add_edit', {
+                title: 'Edit Car',
+                car: carToEdit
+
+            })
+        }
+    });
 }
 
 // Processes the data submitted from the Edit form to update a car
 module.exports.processEditPage = (req, res, next) => {
-    
     // ADD YOUR CODE HERE
-    
+    let id = req.params.id
+
+    let updatedCar = CarModel({
+        _id: req.body.id,
+        make: req.body.make,
+        model: req.body.model,
+        year: req.body.year,
+        kilometers: req.body.kilometers,
+        doors: req.body.doors,
+        seats: req.body.seats,
+        color: req.body.color,
+        price: req.body.price
+    });
+
+    CarModel.updateOne({_id: id}, updatedCar, (err) => {
+        if(err)
+        {
+            console.log(err);
+            res.end(err);
+        }
+        else
+        {
+            // console.log(req.body);
+            // refresh the book list
+            res.redirect('/cars/list');
+        }
+    });
 }
 
 // Deletes a car based on its id.
